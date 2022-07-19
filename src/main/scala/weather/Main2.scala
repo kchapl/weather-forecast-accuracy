3d683f0cccdf5c4f73678baba9ae112c941b5b0e
kchapl <kchapl@users.noreply.github.com>
2022-07-19 20:58:18 +0100

    Keep server going forever

--- src/main/scala/weather/Main2.scala
+++ src/main/scala/weather/Main2.scala
@@ -16,19 +16,19 @@ object Main2 extends ZIOAppDefault {
       s"https://maker.ifttt.com/trigger/weather_reading/with/key/$apiKey?value1=$weatherReport&value2=&value3="
   } yield webHook
 
-  private def startServer(port: Int, webHook: String) = ZIO.attempt {
-    val server = HttpServer.create(new InetSocketAddress(port), 0)
-    server.createContext("/", new RootHandler(webHook))
-    server.setExecutor(null)
-    server.start()
-    server
-  }
+  private def startServer(port: Int, webHook: String) = for {
+    server <- ZIO.attempt(HttpServer.create(new InetSocketAddress(port), 0))
+    _ <- ZIO.attempt(server.createContext("/", new RootHandler(webHook)))
+    _ <- ZIO.attempt(server.setExecutor(null))
+    _ <- ZIO.attempt(server.start())
+  } yield server
 
   private def program = ZIO.scoped(for {
     port <- env("PORT").map(_.toInt)
     webHook <- createWebHook
-    _ <- ZIO.acquireRelease(startServer(port, webHook))(server => ZIO.succeed(server.stop(0)))
-    _ <- Console.readLine
+    _ <- ZIO
+      .acquireRelease(startServer(port, webHook))(server => ZIO.succeed(server.stop(0)))
+    _ <- ZIO.sleep(Duration.fromSeconds(60 * 60))
   } yield ())
 
   override def run: ZIO[Any, Any, Any] = program
